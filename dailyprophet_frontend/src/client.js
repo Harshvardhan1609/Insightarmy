9f394ab339c6b44ab0d2747ea794a049bde1457a
HARSHVARDHAN PUROHIT <75427196+Harshvardhan1609@users.noreply.github.com>
2023-03-08 11:35:25 +0530

    Update client.js

--- dailyprophet_frontend/src/client.js
+++ dailyprophet_frontend/src/client.js
@@ -2,10 +2,10 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
-    projectId: 'k9g4e6dp',
-    dataset: 'production',
-    apiVersion: '2023-01-13',
-    token: 'skT2NscuaI7KvSTEr5UtSpiwKucS1IrHuVIIS9vg2q01kFAio6etS9vzOq2P66nA9GrAwotzxcp9zOTUgamWq6Xwvnrbzhy0L2v28SYV55CNXfjtYgIm1WLAHoeZv6DrhH4ve99eYXSJ3x8jbEQPy3LWx4S4bipJM5afV5c6lXEUMPLB85Ef' ,
+    projectId: 'pid',
+    dataset: 'productionss',
+    apiVersion: '2023-01-15',
+    token: 'Token_Id' ,
     useCdn: true,
 });
 
